4e7da8060644312df6bce5a5fb6ed2f29a0ed03f
Jocelyn Huang <seaweed4@live.com>
2019-11-26 22:27:52 -0500

    returns a list of eng/span dictionaries

--- scraper.py
+++ scraper.py
@@ -3,43 +3,23 @@ from bs4 import BeautifulSoup as bs
 import urllib.request
 import requests
 
-word = "pillar"
-# source = urllib.request.urlopen('https://www.spanishdict.com/translate/pillar')
+word = "gustar"
 
-page = requests.get('https://www.spanishdict.com/translate/pillar')
-# source = urllib.request.urlopen('https://www.spanishdict.com/translate/%s) % word
-
-
-# soup = bs.BeautifulSoup(page, 'lxml')
+page = requests.get('https://www.spanishdict.com/translate/{}'.format(word))
 
 soup = bs(page.content, 'html.parser')
-# print(soup.children[0])
 
-# print(list(soup.children))
+dictionaryContainer = soup.find(id="dictionary-neodict-es")
 
-# print([type(item) for item in list(soup.children)])
+listEnglishSentences = [sentence.get_text()
+                        for sentence in list(dictionaryContainer.find_all('em'))]
+listSpanishSentences = [sentence.previous_sibling.previous_sibling.get_text()
+                        for sentence in list(dictionaryContainer.find_all('em'))]
 
-# html = list(soup.children)[1]
+exampleSentencesDictionary = []
 
-# print(list(html.children)[1])
-
-# print(soup.find_all(id='dictionary-neodict-es').children[0])
-# print(soup.find_all('em'))
-
-enToSp = soup.find(id='translate-en')
-# print(enToSp[0, 10])
-# print([type(item) for item in list(enToSp.children)])
-# print(list(enToSp.children))
-#
-# print(enToSp)
-
-# print([for item in list(enToSp.find_all('em').get_text())
-
-# for eng in enToSp.find_all('em'):
-# print(eng.get_text())
-
-listEmTags = enToSp.find_all('em')
-print([item.get_text() for item in list(listEmTags)])
-
-# print(enToSp).find_all('em')
-print(enToSp.find_all('em')[0])
+for sentence, sentenceES in zip(listEnglishSentences, listSpanishSentences):
+    exampleSentencesDictionary.append({
+        'english': sentence,
+        'spanish': sentenceES
+    })
