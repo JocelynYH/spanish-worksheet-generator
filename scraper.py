1706cec3a73f3028bbd5a361ef150430b31321a1
Jocelyn Huang <seaweed4@live.com>
2019-11-26 21:55:15 -0500

    scrape english sentences

--- scraper.py
+++ scraper.py
@@ -1,4 +1,4 @@
-# import bs4 as 
+# import bs4 as
 from bs4 import BeautifulSoup as bs
 import urllib.request
 import requests
@@ -13,5 +13,33 @@ page = requests.get('https://www.spanishdict.com/translate/pillar')
 # soup = bs.BeautifulSoup(page, 'lxml')
 
 soup = bs(page.content, 'html.parser')
-# print(soup.prettify())
-list(soup.children)
+# print(soup.children[0])
+
+# print(list(soup.children))
+
+# print([type(item) for item in list(soup.children)])
+
+# html = list(soup.children)[1]
+
+# print(list(html.children)[1])
+
+# print(soup.find_all(id='dictionary-neodict-es').children[0])
+# print(soup.find_all('em'))
+
+enToSp = soup.find(id='translate-en')
+# print(enToSp[0, 10])
+# print([type(item) for item in list(enToSp.children)])
+# print(list(enToSp.children))
+#
+# print(enToSp)
+
+# print([for item in list(enToSp.find_all('em').get_text())
+
+# for eng in enToSp.find_all('em'):
+# print(eng.get_text())
+
+listEmTags = enToSp.find_all('em')
+print([item.get_text() for item in list(listEmTags)])
+
+# print(enToSp).find_all('em')
+print(enToSp.find_all('em')[0])
